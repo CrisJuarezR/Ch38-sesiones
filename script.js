2fdaffbe454076d859bbf5a1b91ed0c03050f0d5
Cristian Juarez <jonathanjr228@gmail.com>
2024-02-19 13:24:13 -0600

    ci: funcion saludar persona

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log ("Hola Ch38");
+function enviarSaludo(){
+    alert('¡Hola Ch38!');
+}
+/**
+ * Pedir el nombre de la persona usando un promt
+ * Mostrar un saludo y nombre de la persona con alert
+ */
+function enviarSaludoAPersona() {
+    let message = prompt("Hola usuario dime tu nombre");
+    alert(`Hola ${message}`);
+}
